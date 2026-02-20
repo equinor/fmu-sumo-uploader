f622142e81174b325f64445f8a7e1f22d7d7b5db
Runar Ask Johannessen <89020325+equinor-ruaj@users.noreply.github.com>
2026-02-20 10:13:06 +0100

    Revert "chore: remove unused params from tests"
    This reverts commit c32364f13110f02c7d77a7ae158cc139f0dc99f4.

--- tests/test_uploader.py
+++ tests/test_uploader.py
@@ -193,7 +193,7 @@ def test_initialization(token, case_metadata):
     )
 
 
-def test_manifest(token, case_metadata, manifest_file):
+def test_manifest(token, case_metadata, surface_file, manifest_file):
     """Assert that manifest exists after exporting data"""
     sumoclient = SumoClient(env=ENV, token=token)
 
@@ -213,7 +213,9 @@ def test_manifest(token, case_metadata, manifest_file):
     assert len(manifest) == 1
 
 
-def test_sumo_uploads(token, case_metadata, manifest_file, sumo_uploads_file):
+def test_sumo_uploads(
+    token, case_metadata, surface_file, manifest_file, sumo_uploads_file
+):
     """Assert that sumo uploads log exists after exporting data"""
     sumoclient = SumoClient(env=ENV, token=token)
 
@@ -326,7 +328,12 @@ def test_case(token, case_metadata):
 
 
 def test_case_with_restricted_child(
-    token, case_metadata, surface_file, surface_metadata_file, manifest_file
+    token,
+    case_metadata,
+    surface_file,
+    surface_metadata_file,
+    manifest_file,
+    sumo_uploads_file,
 ):
     """Assert that uploading a child with 'classification: restricted' works.
     Assumes that the identity running this test have enough rights for that."""
@@ -515,7 +522,12 @@ def test_case_with_one_child_and_parameters_txt(
 
 
 def test_case_with_one_child_with_affiliate_access(
-    token, case_metadata, surface_file, surface_metadata_file, manifest_file
+    token,
+    case_metadata,
+    surface_file,
+    surface_metadata_file,
+    manifest_file,
+    sumo_uploads_file,
 ):
     """Upload one file to Sumo with affiliate access.
     Assert that it is there."""
@@ -718,7 +730,7 @@ def test_invalid_yml_in_case_metadata(token):
 
 
 def test_invalid_yml_in_child_metadata(
-    token, case_metadata, surface_file, manifest_file
+    token, case_metadata, surface_file, manifest_file, sumo_uploads_file
 ):
     """
     Try to upload child with invalid yml in its metadata file.
