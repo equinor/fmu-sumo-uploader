33a8b36f6453bf5eb05125deabd9112ba9681cbb
Raymond Wiker <rayw@equinor.com>
2026-02-24 13:38:34 +0100

    Revert "chore: remove dangling references to SumoConnection"
    This reverts commit a628242e9f4c251d9e7687ef48558ff14317a2ed.

--- src/fmu/sumo/uploader/__init__.py
+++ src/fmu/sumo/uploader/__init__.py
@@ -12,5 +12,7 @@ import sumo.wrapper
 from fmu.sumo.uploader.caseondisk import CaseOnDisk
 from fmu.sumo.uploader.caseonjob import CaseOnJob
 
+SumoConnection = sumo.wrapper.SumoClient
+
 # from fmu.sumo.uploader._fileondisk import FileOnDisk
 # from fmu.sumo.uploader._fileonjob import FileOnJob
