153316e7f2ab83147cd257f202d9583232c5f40d
Li Deng <lide@equinor.com>
2026-07-27 12:44:28 +0200

    feat: set ensemble status to scratch when creating ensemble object

--- src/fmu/sumo/uploader/_upload_files.py
+++ src/fmu/sumo/uploader/_upload_files.py
@@ -57,6 +57,7 @@ def maybe_upload_realization_and_ensemble(sumoclient, base_metadata):
             del ensemble_metadata["fmu"]["realization"]
             ensemble_metadata["class"] = "ensemble"
             ensemble_metadata["fmu"]["context"]["stage"] = "ensemble"
+            ensemble_metadata["_sumo"]["status"] = "scratch"
             sumoclient.post(f"/objects('{case_uuid}')", json=ensemble_metadata)
 
         sumoclient.post(f"/objects('{case_uuid}')", json=realization_metadata)
@@ -79,6 +80,7 @@ def maybe_upload_ensemble(sumoclient, base_metadata):
         ensemble_metadata = _base_object_metadata(base_metadata)
         ensemble_metadata["class"] = "ensemble"
         ensemble_metadata["fmu"]["context"]["stage"] = "ensemble"
+        ensemble_metadata["_sumo"]["status"] = "scratch"
 
         case_uuid = ensemble_metadata["fmu"]["case"]["uuid"]
         sumoclient.post(f"/objects('{case_uuid}')", json=ensemble_metadata)
